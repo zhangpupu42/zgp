eccf59eae41b75d63a775ffae109dc64e04db220
Mr.ZhangGP <17600383703@163.com>
2020-12-17 10:50:25 +0800

    首次提交

---
+++ README.md
@@ -0,0 +1,21 @@
+# Zgp
+
+`zgp` 集成 yeoman ，自带 generator-zgp 生成器 .
+
+## Install
+
+```
+$ npm install --global zgp
+```
+
+## Usage
+
+```
+$ zgp
+```
+
+根据选项生成不同的项目，暂时只有生成项目功能，期待后期慢慢补充完善
+
+[zgp-pages](https://github.com/zhangpupu42/zce-pages)
+
+[Zgp Generator](https://github.com/zhangpupu42/generator-zgp)
---
+++ bin/zgp.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+const bases = process.argv.splice(0, 2)
+
+inquirer = require('inquirer')
+
+const firList = [
+  {
+    type: 'list',
+    name: 'action',
+    message: 'What do you want to do?',
+    choices: [
+      {
+        name: 'init a program',
+        value: 'zgp'
+      }
+    ]
+  }
+]
+
+const secList = [
+  {
+    type: 'list',
+    name: 'type',
+    message: 'What kind of type do you want?',
+    choices: [
+      {
+        name: 'mini program',
+        value: 'default'
+      },
+      {
+        name: 'pages-boilerplate program',
+        value: 'poiler'
+      }
+    ]
+  }
+]
+
+function main() {
+  inquirer
+    .prompt(firList)
+    .then((answers) => {
+      forward(answers.action)
+    })
+}
+
+function forward(action) {
+  bases.push(action)
+  inquirer
+    .prompt(secList)
+    .then((answers) => {
+      if(answers.type !== 'default'){
+        bases.push(answers.type)
+      }
+      process.argv = bases
+      require('yo/lib/cli')
+    })
+}
+
+main()
